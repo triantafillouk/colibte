f8abf152f142126caf99878f769c3f707c7ef294
kostas triantafillou <kt@Mac.home>
2026-01-05 16:31:20 +0200

    #01.7T48 (5/1/2026) var opts

--- xe.h
+++ xe.h
@@ -11,7 +11,7 @@
 
 /*	Program Identification..... */
 #define	PROGNAME	"Colibri text editor"
-#define VERSION 	 "#01.7T47 (5/1/2026)"
+#define VERSION 	 "#01.7T48 (5/1/2026)"
 
 
 // merged from kle4 #776T46 (28/7/2022)
