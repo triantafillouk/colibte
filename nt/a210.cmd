09456088debf4644ceee8acdad87283aa00ee51e
kostas triantafillou <kt@Mac.home>
2026-07-26 11:36:02 +0300

    .

--- nt/a210.cmd
+++ nt/a210.cmd
@@ -8,13 +8,18 @@ a=[	1, 2, 3
 	4, 5, 6
 	7, 8, 9]
 
-# a:
-a[1][0]:
-a[1][0]++:
-15:
-# a[1][0]:
-# a:
-# a[1][0]+=3
+# print("original array",a)
+print("original a[0][0]=",a[0][0])
+print("original a[1][0]=",a[1][0])
+a++
+print("after array increase a[1][0]=",a[1][0])
+a[1][0]++
+print("after element increase a[1][0]=",a[1][0])
+a[1][0]*=3
+print("after element mulby 3 a[1][0]=",a[1][0])
+a[1][0]+=2:     7 | 0x7 | 0o7
+print("after element updateby 2 a[1][0]=",a[1][0])
 # a[1][0]
 # a[1][2]=a[1][1]*2+1 :
-print(a)
+print("final a[0][0]=",a[0][0])
+
